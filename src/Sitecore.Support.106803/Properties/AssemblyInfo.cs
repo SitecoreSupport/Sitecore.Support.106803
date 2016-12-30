4de8202e83f69ab7668a6a3292467f638616e021
Dmitry Kolinchuk <dmko@sitecore.net>
2016-12-30 17:59:37 +0200

    Allows to save more than two lines in the Multi-Line Text field in Experience Editor

---
+++ src/Sitecore.Support.106803/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.106803")]
+[assembly: AssemblyProduct("Sitecore.Support.106803")]
+[assembly: ComVisible(false)]
